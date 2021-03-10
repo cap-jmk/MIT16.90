cc3985c3827d5b5ce85625df48e0b0be78421c42
Julian M. Kleber <jkdadww@icloud.com>
2021-03-10 13:06:16 +0100

    Yeah so I added now a solver for 2nd order ODEs, can be generalized but the priciple is now much clearer

--- tests.py
+++ tests.py
@@ -7,9 +7,50 @@ from MIT1690.fallingSphere import (discretize_time, approximate_reynolds, discre
                                    forward_euler, midpoint_rule)
 from MIT1690.accuracy import (find_best_multistep, calculate_error, comparison_function, analytical_solution_comparison,
                               integrate_best_two_point_multistep)
+from MIT1690.pendulum import (integrate_pendulum_feu, integrate_pendulum_mpr, model)
+
 rc('font', **{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 
+
+def test_pendulum_midpoint_rule():
+    """
+
+    :return:
+    :rtype:
+    """
+    initial_time = 0
+    end_time = 10
+    initial_values = [0, np.pi/4]
+    time_step = 0.02
+    grid = discretize_time(initial_value=initial_time, end_value= end_time, step = time_step)
+    results = integrate_pendulum_mpr(grid=grid, initial_values=initial_values, model=model, timestep=time_step)
+    plt.plot(grid, results[0], label="$\omega$")
+    plt.plot(grid, results[1], label="$\ttheta$")
+    plt.legend()
+    plt.xlabel("t [s]")
+    plt.ylabel("v  [m/s]")
+    plt.savefig("plots/2nd_Order_Pendulum_MPR.png", dpi=300)
+    plt.close()
+def test_pendulum_forward_euler():
+    """
+
+    :return:
+    :rtype:
+    """
+    initial_time = 0
+    end_time = 10
+    initial_values = [0, np.pi/4]
+    time_step = 0.02
+    grid = discretize_time(initial_value=initial_time, end_value= end_time, step = time_step)
+    results = integrate_pendulum_feu(grid=grid, initial_values=initial_values, model=model, timestep=time_step)
+    plt.plot(grid, results[0], label="$\omega$")
+    plt.plot(grid, results[1], label="$\ttheta$")
+    plt.legend()
+    plt.xlabel("t [s]")
+    plt.ylabel("v  [m/s]")
+    plt.savefig("plots/2nd_Order_Pendulum_FEU.png", dpi=300)
+    plt.close()
 
 def test_best_two_pint():
     """
